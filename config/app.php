21a3d0eadc3afc4d063725ad9786c7f28db20567
Simonas Poska <Simonas@Simonass-iMac.local>
2018-02-18 11:19:34 +0200

    Debug mode

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -104,7 +104,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'LL/gRKDBcHiPLmuZAIPLiRYyiVtDwJU87cvUPlp5CpM='),
+    'key' => env('APP_KEY'),
 
     'cipher' => 'AES-256-CBC',
 
