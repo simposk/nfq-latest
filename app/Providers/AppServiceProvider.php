e621e7a60c7137bf18505d22775770898e4ff643
Simonas Poska <Simonas@Simonass-iMac.local>
2018-02-18 13:02:05 +0200

    change to the default database character set

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,9 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(env('APP_ENV') == 'production') {
-            \URL::forceScheme('https');
-        }
+        Schema::defaultStringLength(191);
     }
 
     /**
